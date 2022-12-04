44707cb3aeb731947e276a2340fc8885e2437c98
Arina <Athin0>
2022-12-04 16:33:08 +0300

    v2 (some refactoring)

--- pkg/NetVulnService/Server.go
+++ pkg/NetVulnService/Server.go
@@ -25,15 +25,13 @@ func (s *Server) CheckVuln(ctx context.Context, req *protofiles.CheckVulnRequest
 	return &protofiles.CheckVulnResponse{Results: results}, nil
 }
 
-func GetLogic(targets []string, ports []int32) ([]*protofiles.TargetResult, error) {
+func ScanHosts(targets []string, ports []int32) ([]nmap.Host, error) {
 	// Equivalent to
-	// nmap -sV -T4 192.168.0.0/24 with a filter to remove non-RTSP ports.
-	//nmap -T4 -A -v 192.168.0.0/24
+	// nmap -sV -scrips=valnures -T4 192.168.0.0/24 .
 	scanner, err := nmap.NewScanner(
-
 		nmap.WithTargets(targets...), //"localhost""\""+targets[0]+"\""
 		nmap.WithScripts("vulners"),
-		//nmap.WithPorts(ConvInt32toSet(ports)...),
+		nmap.WithPorts(ConvInt32toSet(ports)...),
 		nmap.WithServiceInfo(),
 		nmap.WithTimingTemplate(nmap.TimingAggressive),
 		nmap.WithVersionAll(),
@@ -45,56 +43,47 @@ func GetLogic(targets []string, ports []int32) ([]*protofiles.TargetResult, erro
 					return true
 				}
 			}
-
 			return false
 		}),
 	)
 
 	if err != nil {
 		log.Fatalf("unable to create nmap scanner: %v", err)
+		return nil, err
 	}
 
 	result, _, err := scanner.Run()
 	if err != nil {
 		log.Fatalf("nmap scan failed: %v", err)
+		return nil, err
+	}
+	return result.Hosts, nil
+}
+
+func GetLogic(targets []string, ports []int32) ([]*protofiles.TargetResult, error) {
+	hosts, err := ScanHosts(targets, ports)
+	if err != nil {
+		log.Fatalf("scan hosts failed: %v", err)
+		return nil, err
 	}
 
 	ans := make([]*protofiles.TargetResult, 0)
-	for _, host := range result.Hosts {
+	for _, host := range hosts {
 		fmt.Printf("Host %s\n", host.Addresses[0])
-		targ := &protofiles.TargetResult{Target: string(host.Addresses[0].Addr)}
+		targ := &protofiles.TargetResult{Target: host.Addresses[0].Addr}
 		services := make([]*protofiles.Service, 0)
 
 		for _, port := range host.Ports {
 			vulns := make([]*protofiles.Vulnerability, 0)
 			fmt.Println(port.ID)
 			for _, elem := range port.Scripts {
-				fmt.Println("id: ", elem.ID) //vulners1
-				fmt.Println("output: ", elem.Output)
 				for _, table := range elem.Tables {
+					//table -таблица с таблицей уязвимостей
 					for _, re := range table.Tables {
-						var cvss = float32(0)
-						var idVuln string
-						for _, values := range re.Elements {
-							if values.Key == "id" {
-								fmt.Println("id:", values.Value)
-								idVuln = values.Value
-							}
-							if values.Key == "cvss" {
-								fmt.Println("cvss:", values.Value)
-								if cvssTample, err := strconv.ParseFloat(values.Value, 32); err == nil {
-									cvss = float32(cvssTample)
-								} else {
-									log.Printf("err in parse %d:", err)
-								}
-							}
-						}
-						vulns = append(vulns, &protofiles.Vulnerability{Identifier: idVuln, CvssScore: cvss})
-
+						vulnerability := GetVulner(&re)
+						vulns = append(vulns, vulnerability)
 					}
 				}
-
-				println()
 			}
 			var version string
 			if len(port.Service.Product) > 0 {
@@ -108,10 +97,30 @@ func GetLogic(targets []string, ports []int32) ([]*protofiles.TargetResult, erro
 
 	return ans, nil
 }
+
+func GetVulner(re *nmap.Table) *protofiles.Vulnerability {
+	var cvss = float32(0)
+	var idVuln string
+	for _, values := range re.Elements {
+		if values.Key == "id" {
+			fmt.Println("id:", values.Value)
+			idVuln = values.Value
+		}
+		if values.Key == "cvss" {
+			fmt.Println("cvss:", values.Value)
+			if cvssTample, err := strconv.ParseFloat(values.Value, 32); err == nil {
+				cvss = float32(cvssTample)
+			} else {
+				log.Printf("err in parse %d:", err)
+			}
+		}
+	}
+	return &protofiles.Vulnerability{Identifier: idVuln, CvssScore: cvss}
+}
 func ConvInt32toSet(in []int32) (out []string) {
 	out = make([]string, len(in))
 	for i, a := range in {
-		out[i] = fmt.Sprint(a)
+		out[i] = strconv.Itoa(int(a))
 	}
 	return out
 }
